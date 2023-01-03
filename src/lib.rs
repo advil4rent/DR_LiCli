048debdb438dc9727dca7089748bbd855c6e2a9e
Bao Le <uac6qw@virginia.edu>
2023-01-03 11:09:16 -0500

    2ac prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,48 @@
 use decide_protocol::{
-    ComponentName, ComponentRequest, proto::{ComponentParams, reply, Reply, StateChange}, REQ_ENDPOINT, Request,
+    ComponentName, ComponentRequest,
+    proto::{ComponentParams, reply, Reply, StateChange},
+    REQ_ENDPOINT, Request,
     RequestType,
 };
-use house_light::proto as hl_proto;
-use peckboard::proto as pb_proto;
+
+
 use prost;
 use prost_types::Any;
 use prost::Message;
 use tmq::{Context, Multipart, Result};
 
-const HS_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/house_light_state";
-const HS_STATE_TYPE_URL: &'static str = "melizalab.org/proto/house_light_state";
-const PL_STATE_TYPE_URL: &'static str = "melizalab.org/proto/led_state";
-const PL_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/led_params";
+/*
 
-pub async fn hs_set_parameters() {
-    let params = Any {
-        type_url: String::from(HS_PARAMS_TYPE_URL),
-        value: hl_proto::Params {
-            clock_interval: 300
-        }.encode_to_vec(),
-    };
+*/
+
+pub async fn set_params(component: &str, params: Any) {
+
     let params_message = ComponentParams {
         parameters: Some(params.clone()),
     };
     let request = Request {
         request_type: RequestType::Component(ComponentRequest::SetParameters),
-        component: Some(ComponentName(String::from("house-light"))),
+        component: Some(ComponentName::from(component)),
         body: params_message.encode_to_vec(),
     };
     let result = send_request(request).await.unwrap();
     assert_eq!(result, reply::Result::Ok(()));
     let request = Request {
         request_type: RequestType::Component(ComponentRequest::GetParameters),
-        component: Some(ComponentName::from("house-light")),
+        component: Some(ComponentName::from(component)),
         body: vec![],
     };
     let result = send_request(request).await.unwrap();
     assert_eq!(result, reply::Result::Params(params));
 }
-pub async fn hs_set_state() {
-    let state = Any {
-        type_url: String::from(HS_STATE_TYPE_URL),
-        value: hl_proto::State {
-            switch: true,
-            fake_clock: true,
-            brightness: 255,
-        }.encode_to_vec(),
-    };
+
+pub async fn set_state(component: &str, state: Any) {
     let state_message = StateChange {
         state: Some(state.clone()),
     };
     let request = Request {
         request_type: RequestType::Component(ComponentRequest::ChangeState),
-        component: Some(ComponentName::from("house-light")),
+        component: Some(ComponentName::from(component)),
         body: state_message.encode_to_vec(),
     };
     // the subscriber must be initialized before the state change is
@@ -61,51 +50,6 @@ pub async fn hs_set_state() {
     let result = send_request(request).await.unwrap();
     assert_eq!(result, reply::Result::Ok(()));
 }
-pub async fn pl_set_parameters() {
-    let params = Any {
-        type_url: String::from(PL_PARAMS_TYPE_URL),
-        value: pb_proto::LedParams {
-        }.encode_to_vec(),
-    };
-    let params_message = ComponentParams {
-        parameters: Some(params.clone()),
-    };
-    let request = Request {
-        request_type: RequestType::Component(ComponentRequest::SetParameters),
-        component: Some(ComponentName(String::from("peck-leds-left"))),
-        body: params_message.encode_to_vec(),
-    };
-    let result = send_request(request).await.unwrap();
-    assert_eq!(result, reply::Result::Ok(()));
-    let request = Request {
-        request_type: RequestType::Component(ComponentRequest::GetParameters),
-        component: Some(ComponentName::from("peck-leds-left")),
-        body: vec![],
-    };
-    let result = send_request(request).await.unwrap();
-    assert_eq!(result, reply::Result::Params(params));
-}
-pub async fn pl_set_state() {
-    let state = Any {
-        type_url: String::from(PL_STATE_TYPE_URL),
-        value: pb_proto::LedState {
-            led_state: String::from("red")
-        }.encode_to_vec(),
-    };
-    let state_message = StateChange {
-        state: Some(state.clone()),
-    };
-    let request = Request {
-        request_type: RequestType::Component(ComponentRequest::ChangeState),
-        component: Some(ComponentName::from("peck-leds-left")),
-        body: state_message.encode_to_vec(),
-    };
-    // the subscriber must be initialized before the state change is
-    // sent because the publish socket doesn't buffer messages
-    let result = send_request(request).await.unwrap();
-    assert_eq!(result, reply::Result::Ok(()));
-}
-
 
 async fn send_request(message: Request) -> Result<reply::Result> {
     let ctx = Context::new();
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
 use decide_protocol::{proto, PUB_ENDPOINT};
 use futures::stream::StreamExt;
-use house_light::proto as hl_proto;
-use peckboard::proto as pb_proto;
 use prost::Message;
 use tmq::{Context, subscribe};
 use tokio;
 use tracing;
+
+use rusty_2ac;
+use house_light::proto as hl_proto;
+use peckboard::proto as pb_proto;
+use sound_alsa::proto as sa_proto;
+use stepper_motor::proto as sm_proto;
+
+const HS_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/house_light_state";
+const HS_STATE_TYPE_URL: &'static str = "melizalab.org/proto/house_light_state";
+const PL_STATE_TYPE_URL: &'static str = "melizalab.org/proto/led_state";
+const PL_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/led_params";
+const SA_STATE_TYPE_URL: &'static str = "melizalab.org/proto/sound_alsa_state";
+const SA_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/sound_alsa_params";
+const SM_TATE_TYPE_URL: &'static str = "melizalab.org/proto/stepper_motor_state";
+const SM_PARAMS_TYPE_URL: &'static str = "melizalab.org/proto/stepper_motor_params";
+
+
+struct Experiment {
+    subject: String,
+    addr: String,
+    experiment: String,
+    trial: u32,
+    stim: String,
+    correction: u32,
+    lights: String,
+    time: String,
+    response: String,
+    correct: bool,
+    result: String,
+    rt: u32,
+}
+
+const MAX_CORRECTION: u8 = 50;
+const AUDIO_DIR: String = String::from("~/colony-noise-stimuli/stimuli/clean-stim");
+const MOTOR_TIMEOUT: u64 = 4000;
+const LIGHTSOUT_DUR: u64 = 10000;
+const EXPERIMENT: String = String::from("2ac_rs");
+const CORRECTION_LIGHT: String = String::from("blue");
 
 #[tokio::main]
 async fn main() {
@@ -16,10 +52,48 @@ async fn main() {
         .with_max_level(tracing::Level::TRACE)
         // sets this to be the default, global collector for this application.
         .init();
-    decide_rs_lights::hs_set_parameters().await;
-    decide_rs_lights::hs_set_state().await;
-    decide_rs_lights::pl_set_parameters().await;
-    decide_rs_lights::pl_set_state().await;
+
+    let hs_param = prost_types:: Any {
+        type_url: String::from(HS_PARAMS_TYPE_URL),
+        value: hl_proto::Params {
+            clock_interval: 300
+        }.encode_to_vec(),
+    };
+
+    let pl_param = prost_types:: Any {
+        type_url: String::from(PL_STATE_TYPE_URL),
+        value: pb_proto::KeyParams {}.encode_to_vec(),
+    };
+
+    let sm_param = prost_types:: Any {
+        type_url: String::from(SM_PARAMS_TYPE_URL),
+        value: sm_proto::Params {
+            timeout: MOTOR_TIMEOUT
+        }.encode_to_vec(),
+    };
+    let sa_param = prost_types:: Any {
+        type_url: String::from(SA_PARAMS_TYPE_URL),
+        value: sa_proto::Params {
+            audio_dir: AUDIO_DIR,
+        }.encode_to_vec(),
+    };
+
+    rusty_2ac::set_params("house-light", hs_param);
+    rusty_2ac::set_params("stepper-motor", sm_param);
+    rusty_2ac::set_params("sound-alsa", sa_param);
+    rusty_2ac::set_params("peck-leds-left", pl_param);
+
+    let hl_state = prost_types::Any {
+        type_url: String::from(HS_PARAMS_TYPE_URL),
+        value: hl_proto::State {
+            switch: true,
+            light_override: false,
+            ephemera: false,
+            brightness: 0,
+        }.encode_to_vec(),
+    };
+    rusty_2ac::set_state("house-light", hl_state);
+
     let ctx = Context::new();
     let mut hs_listen = subscribe(&ctx)
         .connect(PUB_ENDPOINT).unwrap()
@@ -28,31 +102,30 @@ async fn main() {
     let mut pb_listen = subscribe(&ctx)
         .connect(PUB_ENDPOINT).unwrap()
         .subscribe(b"state/peck-keys").unwrap();
+    let mut sm_listen = subscribe(&ctx)
+        .connect(PUB_ENDPOINT).unwrap()
+        .subscribe(b"state/stepper-motor").unwrap();
+    let mut sa_listen = subscribe(&ctx)
+        .connect(PUB_ENDPOINT).unwrap()
+        .subscribe(b"state/sound-alsa").unwrap();
+
+    let current = Experiment {
+        subject: "F5".to_string(),
+        addr: "beagle-0".to_string(),
+        experiment: EXPERIMENT,
+        trial: 0,
+        stim: "pmkimwop_30".to_string(),
+        correction: 0,
+        lights: "none".to_string(),
+        time: "".to_string(),
+        response: "timeout".to_string(),
+        correct: false,
+        result: "none".to_string(),
+        rt: 0,
+    };
+
     loop {
-        tokio::select! {
-            hs_state = hs_listen.next() => {
-                let hs_state = hs_state.map(|message|{
-                    let mut message = message.unwrap();
-                    tracing::info!("received house-light pub");
-                    let _topic = message.pop_front().unwrap();
-                    let encoded_pub = message.pop_front().unwrap();
-                    let decoded_pub = proto::Pub::decode(&encoded_pub[..]).expect("could not decode protobuf");
-                    hl_proto::State::decode(&*decoded_pub.state.unwrap().value)
-                    .expect("could not decode state change")})
-                .unwrap();
-                println!("Changed house-light state: switch {:?}, fake_clock {:?}, brightness {:?}", &hs_state.switch,  &hs_state.fake_clock, &hs_state.brightness)}
-            pb_state = pb_listen.next() => {
-                let pb_state = pb_state.map(|message|{
-                    let mut message = message.unwrap();
-                    tracing::info!("received peck-keys pub");
-                    let _topic = message.pop_front().unwrap();
-                    let encoded_pub = message.pop_front().unwrap();
-                    let decoded_pub = proto::Pub::decode(&encoded_pub[..]).expect("could not decode protobuf");
-                    pb_proto::KeyState::decode(&*decoded_pub.state.unwrap().value)
-                    .expect("could not decode state change")})
-                .unwrap();
-                println!("Changed peck-key state: left {:?}, center {:?}, right {:?}", &pb_state.peck_left,  &pb_state.peck_center, &pb_state.peck_right)}
-        }
+
     }
 }
 
